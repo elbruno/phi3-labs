0400dfeba2c6b5d7ff02151b4f924318a18de706
Bruno Capuano <bcapuano@gmail.com>
2024-06-03 17:09:58 -0400

    testing phi-3 vision onnx

--- src/LabsPhi303/Program.cs
+++ src/LabsPhi303/Program.cs
@@ -1,41 +1,36 @@
-﻿using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.ChatCompletion;
+﻿using System;
+using System.Reflection.Emit;
+using System.Reflection;
+using Microsoft.ML.OnnxRuntimeGenAI;
 
+
+var modelPath = @"d:\phi3\models\Phi-3-vision-128k-instruct-onnx-cpu\cpu-int4-rtn-block-32-acc-level-4";
+var model = new Model(modelPath);
+var tokenizer = new Tokenizer(model);
 
 var systemPrompt = "You are an AI assistant that helps people find information. Answer questions using a direct style. Do not share more information that the requested by the users.";
-var userQuestion = "What is the capital of France?";
-var assistantResponse = "The capital of France is Paris.";
 
-var modelPath = @"D:\phi3\models\Phi-3-mini-4k-instruct-onnx\cpu_and_mobile\cpu-int4-rtn-block-32";
+var imageUrl = @"https://github.com/elbruno/gpt4ol-sk-csharp/blob/main/imgs/rpi5.png?raw=true";
+var describeImageQuestion = " ";
 
-// create kernel
-var builder = Kernel.CreateBuilder();
-builder.add (modelPath, vocabPath) //AddOnnxRuntimeGenAIChatCompletion(modelPath: modelPath);
-var kernel = builder.Build();
+// show phi3 response
+Console.Write("Phi3: ");
+var fullPrompt = $"<|system|>{systemPrompt}<|end|><|user|>{describeImageQuestion}<|end|><|assistant|>";
+var tokens = tokenizer.Encode(fullPrompt);
 
-// create chat
-var chat = kernel.GetRequiredService<IChatCompletionService>();
-var history = new ChatHistory();
+var generatorParams = new GeneratorParams(model);
+generatorParams.SetSearchOption("max_length", 2048);
+generatorParams.SetSearchOption("past_present_share_buffer", false);
+generatorParams.SetInputSequences(tokens);
 
-// run chat
-while (true)
+var generator = new Generator(model, generatorParams);
+while (!generator.IsDone())
 {
-    Console.Write("Q: ");
-    var userQ = Console.ReadLine();
-    if (string.IsNullOrEmpty(userQ))
-    {
-        break;
-    }
-    history.AddUserMessage(userQ);
-
-    Console.Write($"Phi3: ");
-    var response = "";
-    var result = chat.GetStreamingChatMessageContentsAsync(history);
-    await foreach (var message in result)
-    {
-        Console.Write(message.Content);
-        response += message.Content;
-    }
-    history.AddAssistantMessage(response);
-    Console.WriteLine("");
+    generator.ComputeLogits();
+    generator.GenerateNextToken();
+    var outputTokens = generator.GetSequence(0);
+    var newToken = outputTokens.Slice(outputTokens.Length - 1, 1);
+    var output = tokenizer.Decode(newToken);
+    Console.Write(output);
 }
+Console.WriteLine();
---
+++ src/LabsPhi304/Program.cs
@@ -0,0 +1,42 @@
+﻿using feiyun0112.SemanticKernel.Connectors.OnnxRuntimeGenAI;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+
+var systemPrompt = "You are an AI assistant that helps people find information. Answer questions using a direct style. Do not share more information that the requested by the users.";
+var userQuestion = "What is the capital of France?";
+var assistantResponse = "The capital of France is Paris.";
+
+var modelPath = @"d:\phi3\models\Phi-3-vision-128k-instruct-onnx-cpu\cpu-int4-rtn-block-32-acc-level-4\";
+
+// create kernel
+var builder = Kernel.CreateBuilder();
+builder.AddOnnxRuntimeGenAIChatCompletion(modelPath: modelPath);
+var kernel = builder.Build();
+
+// create chat
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+var history = new ChatHistory();
+
+// run chat
+while (true)
+{
+    Console.Write("Q: ");
+    var userQ = Console.ReadLine();
+    if (string.IsNullOrEmpty(userQ))
+    {
+        break;
+    }
+    history.AddUserMessage(userQ);
+
+    Console.Write($"Phi3: ");
+    var response = "";
+    var result = chat.GetStreamingChatMessageContentsAsync(history);
+    await foreach (var message in result)
+    {
+        Console.Write(message.Content);
+        response += message.Content;
+    }
+    history.AddAssistantMessage(response);
+    Console.WriteLine("");
+}
